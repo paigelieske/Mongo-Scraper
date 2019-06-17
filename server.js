c942edbdc5329b85ecd046ff26a9b59975dbb862
Paige Lieske <paigeproctor@comcast.net>
2019-06-17 12:45:38 -0400

    models added

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-mongoose.connect(MONGODB_URI);
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
