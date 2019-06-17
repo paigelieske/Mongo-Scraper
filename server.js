148b35f5d14f67c45e8c4bc6efd09b2ff962ad3b
Paige Lieske <paigeproctor@comcast.net>
2019-06-17 14:39:28 -0400

    scrape working

--- server.js
+++ server.js
@@ -13,11 +13,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+app.get("/scrape", function (req, res) {
+    axios.get("http://www.bitchesgetriches.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        $("article h2").each(function (i, element) {
+            
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
 });
 
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
